a32844b42ccc44fcdcd3d0efd9c93c2475fb6395
Christophe Lucas <clucas@opnet.com>
2012-12-19 00:25:53 -0500

    change destination of config file

--- lib/myipv4.rb
+++ lib/myipv4.rb
@@ -5,12 +5,13 @@ require 'yaml'
 
 module Myipv4
 
-  config = YAML.load_file(File.join(File.dirname(__FILE__),"myipv4/yaml/ip_resolvers.yml"))
+  config = YAML.load_file(File.join(File.dirname(__FILE__),"../config/ip_resolvers.yml"))
   MIN_REQUEST = config[:minimum_requests]
   URLS = config[:sites]
   http_requests = []
   res_ips = []
   ips = []
+
   EventMachine.run  do
     URLS.each do |url|
       http_requests << EventMachine::HttpRequest.new(url).get
@@ -19,8 +20,7 @@ module Myipv4
     http_requests.each do |http_request|
       http_request.callback {
         if http_request.response_header.http_status.eql?(200)
-          ip = http_request.response.scan(/\d+.\d+.\d+.\d+/)
-          res_ips << ip
+          res_ips << http_request.response.scan(/\d+.\d+.\d+.\d+/)
         end
         ips = res_ips.uniq
         if res_ips.size >= MIN_REQUEST  && ips.size < res_ips.size-1
